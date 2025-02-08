6bc7765543409006d1076b3ca4618ee6d824d34c
Eric <reinprechteric@gmail.com>
2025-02-08 10:54:47 +0100

    firebase

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="main">
       <BasePage>
         <h1>This is the Home Page !</h1>
+        <div className="lol">{process.env.TEST}</div>
       </BasePage>
     </div>
   );
