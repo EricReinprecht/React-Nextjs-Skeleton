68c3e4f6d8de44a9178bc418cd4745725770efc7
Eric <reinprechteric@gmail.com>
2025-05-29 13:00:31 +0200

    create party icon

--- src/app/(pages)/(user)/profile/page.tsx
+++ src/app/(pages)/(user)/profile/page.tsx
@@ -6,7 +6,7 @@ import "@styles/pages/profile.scss"
 import Gear from "@svgs/gear";
 import Link from "next/link";
 import PartyIcon from "@/src/app/lib/svgs/myParties";
-import NewPartyIcon from "@/src/app/lib/svgs/new_party_icon";
+import CreateParty from "@/src/app/lib/svgs/create_party";
 
 const Profile: React.FC = () => {
     return (
@@ -16,7 +16,7 @@ const Profile: React.FC = () => {
                     <div className="grid">
                         <div className="item"><Link href={"/profile/settings"}><Gear></Gear></Link></div>
                         <div className="item"><Link href={"/profile/my-parties"}><PartyIcon></PartyIcon></Link></div>
-                        <div className="item"><Link href={"/profile/settings"}><NewPartyIcon></NewPartyIcon></Link></div>
+                        <div className="item"><Link href={"/profile/settings"}><CreateParty></CreateParty></Link></div>
                         <div className="item"><Link href={"/profile/settings"}><Gear></Gear></Link></div>
                         <div className="item"><Link href={"/profile/settings"}><Gear></Gear></Link></div>
                         <div className="item"><Link href={"/profile/settings"}><Gear></Gear></Link></div>
